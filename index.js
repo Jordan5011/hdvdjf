1b9d05e27652acb69fa9cf8cdc53f4529b57e2d8
Jordan5011 <114581364+Jordan5011@users.noreply.github.com>
2024-07-27 04:02:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const bot = mineflayer.createBot({
   host: 'jdodbrn.aternos.me', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
   username: 'SG_SHOWRIYA', 
-  port: 29740, 
+  port: 28581, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
